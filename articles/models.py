3b4c3794978c8ccb2fd6cb4198ed43ecc9853e7a
Olo Panganiban <olopangs@gmail.com>
2020-11-26 06:43:06 +0800

    End of Chapter 6

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models.fields import SlugField
 
 # Create your models here.
+# class Article(models.Model): // table name: Article, columns = yung variables 
+# https://docs.djangoproject.com/en/3.1/ref/models/fields/#model-field-types
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True) # auto add time 
+    # add in thumbnail later
+    # add in author later
