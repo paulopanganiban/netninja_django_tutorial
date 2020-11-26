edfc3339e9d65237a9621e5916c11854bc0b694a
Olo Panganiban <olopangs@gmail.com>
2020-11-27 07:43:26 +0800

    End of Chapter 15

--- articles/urls.py
+++ articles/urls.py
@@ -1,5 +1,17 @@
 from django.urls import path, include
 from . import  views
+
+app_name = 'articles'
+# bakit importante tong namespace? para hindi matanga si template.
+# para pag nag call ng url, articles:detail
+# so parang sa html, app_name:name / articles:detail
 urlpatterns = [
-    path('', views.article_list)
+    path('', views.article_list, name='list'),
+                                # {% url 'list' %}
+    path('<slug:slug>', views.article_detail, name='detail'),
+    # '<slug: is the TYPE of the PASSED parameter
+    # <slug ay yung pinasa ng user
+    # :slug> name ng parameter na gagamitin sa url na pwedeng articles/detail/asdasdasd --> asdasd iqquery natin sa views.py
+
+    # para saan yung name? para magets ng HTML template! fucking cool.
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,24 @@
+from django.http.response import Http404, HttpResponse
 from django.shortcuts import render
 from .models import Article
+from django.http.response import HttpResponse, Http404
 # Create your views here.
+
 
 def article_list(request):
     articles = Article.objects.all().order_by('date')
     # pass the articles / convert first to a dictionary
-    context = {'articlesOLO' : articles}
+    context = {'articlesOLO': articles}
     return render(request, 'articles/article_list.html', context)
+
+
+def article_detail(request, slug):
+    # 
+    # gagamit tayo ng query
+    # try:
+    #     detail = Article.objects.get(pk=slug)
+    # except Article.DoesNotExist:
+    #     raise Http404
+    # context = {'detail': detail}
+    # return render(request, 'articles/article_detail.html', context)
+    return HttpResponse(slug)
