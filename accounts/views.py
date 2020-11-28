3e0f9a865d4026e5aced09a51db6301ea54e3274
Olo Panganiban <olopangs@gmail.com>
2020-11-28 17:15:05 +0800

    End of chapter 19

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http.response import Http404, HttpResponse
+
+# Django auth 
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 
 def signup_view(request):
-    # return HttpResponse('test')
-    str = "HOLLA"
-    context = {'str' : str}
-    return render(request, 'accounts/signup_view.html')
+    form = UserCreationForm()
+    context = {'form' : form}
+    return render(request, 'accounts/signup_view.html', context)
