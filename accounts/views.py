0be995f1a249b8f59bf47e546c45a30a2502fad8
Olo Panganiban <olopangs@gmail.com>
2020-11-28 18:08:29 +0800

    End of chapter 20

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http.response import Http404, HttpResponse
 
-# Django auth 
+# Django auth
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 
 def signup_view(request):
-    form = UserCreationForm()
-    context = {'form' : form}
+    if request.method == "POST":
+        # take the data and validate
+        # create an instance and access the request.POST's data
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # if true
+            form.save()  # save that data to the database @_@ wowowowowow!
+            # log the user in
+            # app_name='articles' url name='list'
+            return redirect('articles:list')
+    else:
+        form = UserCreationForm()
+    context = {'form': form}
     return render(request, 'accounts/signup_view.html', context)
