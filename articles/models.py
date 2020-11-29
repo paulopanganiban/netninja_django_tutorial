cbe6eeb527faf68a5827bca8978f8f644a89552d
Olo Panganiban <olopangs@gmail.com>
2020-11-30 07:13:04 +0800

    End of chapter 27

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.db.models.fields import SlugField
-
+from django.contrib.auth.models import User
 # Create your models here.
 # class Article(models.Model): // table name: Article, columns = yung variables 
 # https://docs.djangoproject.com/en/3.1/ref/models/fields/#model-field-types
@@ -12,7 +12,7 @@ class Article(models.Model):
     # add in thumbnail
     thumbnail = models.ImageField(default='default.png', blank=True)
     # add in author later
-
+    author = models.ForeignKey(User, on_delete=models.CASCADE,default=None)
     def __str__(self):
         return self.title
     
